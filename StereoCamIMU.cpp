f043b75fb3df9d0b9ed87de56809590d96c440bd
djblazkowicz <40417349@live.napier.ac.uk>
2023-01-15 20:26:54 +0000

    ov9281 + bno055

--- StereoCamIMU.cpp
+++ StereoCamIMU.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 //#include <windows.h>
 #include "StereoCamIMU.h"
 #include <thread>
@@ -7,16 +8,37 @@
 #include <chrono>
 #include <string>
 
-void getIMU()
+int main()
 {
-    using namespace std::literals::chrono_literals;
-    newIMUAvailable = false;
 
+    int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+    cv::VideoCapture cap1(0, cv::CAP_DSHOW);
+    cap1.set(cv::CAP_PROP_FOURCC, codec);
+    cap1.set(cv::CAP_PROP_AUTO_EXPOSURE, 0);
+    cap1.set(cv::CAP_PROP_EXPOSURE, -6);
+    cap1.set(cv::CAP_PROP_MONOCHROME, 1);
+    cap1.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap1.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    cv::VideoCapture cap2(1, cv::CAP_DSHOW);
+    cap2.set(cv::CAP_PROP_FOURCC, codec);
+    cap2.set(cv::CAP_PROP_AUTO_EXPOSURE, 0);
+    cap2.set(cv::CAP_PROP_EXPOSURE, -6);
+    cap2.set(cv::CAP_PROP_MONOCHROME, 1);
+    cap2.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap2.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    cap1.set(cv::CAP_PROP_SETTINGS, 0);
+    cap2.set(cv::CAP_PROP_SETTINGS, 0);
+
+    cv::namedWindow("full frame");
+    cv::namedWindow("left image");
+    cv::namedWindow("right image");
 
     serial::Serial IMU;
     bool IMUavailable = false;
 
-    IMU.setPort("COM12");
+    IMU.setPort("COM8");
     IMU.setBaudrate(115200);
     try {
         IMU.open();
@@ -24,7 +46,6 @@ void getIMU()
     catch (...)
     {
     }
-
     while (true)
     {
         newIMUAvailable = false;
@@ -47,6 +68,7 @@ void getIMU()
                             read_vals[3] = 0;
                             read_vals[4] = 0;
                             read_vals[5] = 0;
+                            read_vals[6] = 0;
 
                             size_t pos = 0;
                             pos = raw_str.find(',');
@@ -69,126 +91,49 @@ void getIMU()
                             read_vals[4] = std::stod(raw_str.substr(0, pos));
                             raw_str.erase(0, pos + 1);
 
+                            pos = raw_str.find(',');
+                            read_vals[5] = std::stod(raw_str.substr(0, pos));
+                            raw_str.erase(0, pos + 1);
 
-                            read_vals[5] = std::stod(raw_str);
+                            read_vals[6] = std::stod(raw_str);
 
                         }
                     }
 
                 }
 
-                gyro[0] = read_vals[0];
-                gyro[1] = read_vals[1];
-                gyro[2] = read_vals[2];
-                accel[0] = read_vals[3];
-                accel[1] = read_vals[4];
-                accel[2] = read_vals[5];
+                frametype = read_vals[0];
+                gyro[0] = read_vals[1];
+                gyro[1] = read_vals[2];
+                gyro[2] = read_vals[3];
+                accel[0] = read_vals[4];
+                accel[1] = read_vals[5];
+                accel[2] = read_vals[6];
 
-                newIMUAvailable = true;
-                std::this_thread::sleep_for(5ms);
+                std::cout << frametype << "," << gyro[0] << "," << gyro[1] << "," << gyro[2] << accel[0] << "," << accel[1] << "," << accel[2] << "\n";
+
+                if (frametype == 2)
+                {
+                    if (!cameraOn) { cameraOn = true; }
+                    cap1 >> right;
+                    cap2 >> left;
+                    cv::cvtColor(left, grayleft,cv::COLOR_BGR2GRAY);
+                    cv::cvtColor(right, grayright, cv::COLOR_BGR2GRAY);
+
+                    cv::hconcat(grayleft, grayright, frame);
+                    cv::imshow("full frame", frame);
+                    cv::imshow("left image", grayleft);
+                    cv::imshow("right image", grayright);
+                }
+
             }
             catch (...)
             {
-                newIMUAvailable = false;
+                //pass
             }
-        }
-        else
-        {
-            newIMUAvailable = false;
-        }
 
-    }
-}
-
-
-void getFrame()
-{
-    using namespace std::literals::chrono_literals;
-
-    
-
-    double delta = 16;
-    double elapsed_time;
-
-    int codec = cv::VideoWriter::fourcc('m', 'j', 'p', '2');
-    cv::VideoCapture cap(0);
-    cap.set(cv::CAP_PROP_AUTO_EXPOSURE, 1);
-    //cap.set(cv::CAP_PROP_EXPOSURE, -6);
-    cap.set(cv::CAP_PROP_FOURCC, codec);
-    cap.set(cv::CAP_PROP_FPS, 60);
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    
-    
-    cv::namedWindow("full frame");
-    cv::namedWindow("left image");
-    cv::namedWindow("right image");
-
-    auto frameStart = std::chrono::system_clock::now();
-    bool tookFirstFrame = false;
-    while (true)
-    {
-
-        newFrameAvailable = false;
-        cap >> frame;
-        if (!tookFirstFrame)
-        {
-            auto frameStart = std::chrono::system_clock::now();
-        }
-
-        cameraOn = true;
-
-        cv::imshow("full frame", frame);
-
-        auto stopwatch = std::chrono::system_clock::now();
-        elapsed_time = double(std::chrono::duration_cast <std::chrono::milliseconds> (stopwatch - frameStart).count());
-        if (elapsed_time >= delta)
-        {
-            left = frame(cv::Rect(0, 0, 640, 480));
-            right = frame(cv::Rect(640, 0, 640, 480));
-            cv::imshow("left image", left);
-            cv::imshow("right image", right);
-            newFrameAvailable = true;
-            tookFirstFrame = true;
-            auto frameStart = std::chrono::system_clock::now();
         }
         char c = (char)cv::waitKey(1);
-    }        
-}
-
-int main()
-{
-    cameraOn = false;
-    using namespace std::chrono_literals;
-   
-    auto start = std::chrono::system_clock::now();
-    double elapsed_time;
-
-    std::thread camWorker(getFrame);
-    std::cout << "waiting for camera to initialize..." << std::endl;
-
-    while(!cameraOn)
-    {
-        std::this_thread::sleep_for(1ms);
     }
-
-    std::thread imuWorker(getIMU);
-
-    while (true)
-    {
-        if (newFrameAvailable)
-        {
-            auto end = std::chrono::system_clock::now();
-            elapsed_time = double(std::chrono::duration_cast <std::chrono::milliseconds> (end - start).count());
-            std::cout << "frame timestamp:" << elapsed_time / 1000.0 << std::endl;
-        }
-        if (newIMUAvailable)
-        {
-            auto end = std::chrono::system_clock::now();
-            elapsed_time = double(std::chrono::duration_cast <std::chrono::milliseconds> (end - start).count());
-            std::cout << "imu timestamp:" << elapsed_time / 1000.0 << std::endl;
-        }
-    }
-
     return 0;
 }
--- StereoCamIMU.h
+++ StereoCamIMU.h
@@ -6,23 +6,20 @@
 #include "serial/serial.h"
 
 cv::Mat frame;
+cv::Mat grayleft;
+cv::Mat grayright;
 cv::Mat left;
 cv::Mat right;
 
 bool newFrameAvailable;
 bool newIMUAvailable;
-bool cameraOn;
+bool cameraOn = false;
 bool turnOnIMU;
+bool getNewFrame = false;
 
 std::string IMUstring;
 std::vector<std::string> words;
-double read_vals[6];
+double read_vals[7];
+int frametype;
 double accel[3];
 double gyro[3];
-
-void getFrame();
-void getImu();
-
-
-
-
